fdd54a68bd99b9372d00352f04e91ccf9a8b6d9d
amanda8412383 <amanda8412383yahoo@gmail.com>
2025-06-18 22:54:12 +0200

    reading estimation plan fix typo

--- estimation plan.sql
+++ estimation plan.sql
@@ -3,4 +3,11 @@ from saleslt.address a
 cross join saleslt.address b
 --estimated execute plan
 --execution run from left to right
---hour thickness = number of rows
+--arrow thickness = number of rows
+--more estimated row requires more memory reserve
+
+set showplan_all on
+go
+
+set showplan_all off
+go
